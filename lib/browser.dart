52794d21af790140f2040a357e7cbc4dc70c56b5
Feng Zhou <fengzhou@gmail.com>
2021-12-05 15:55:04 +0800

    Add icons

--- lib/browser.dart
+++ lib/browser.dart
@@ -26,6 +26,28 @@ class BrowserWidget extends StatelessWidget {
     }
   }
 
+  static Icon iconmap(dynamic item) {
+    const Icon album = Icon(Icons.album_outlined);
+    const Icon computer = Icon(Icons.computer_outlined);
+    const Icon artist = Icon(Icons.person);
+    const Icon favorite = Icon(Icons.favorite);
+    const Icon playlists = Icon(Icons.playlist_play);
+    const Icon library = Icon(Icons.music_note);
+    const Icon last100 = Icon(Icons.history);
+    const Icon radio = Icon(Icons.radio);
+    const Icon genres = Icon(Icons.category);
+    String uri = item['uri'];
+    if (uri.startsWith('favourites')) return favorite;
+    if (uri.startsWith('music-library')) return library;
+    if (uri.startsWith('playlists')) return playlists;
+    if (uri.startsWith('artists://')) return artist;
+    if (uri.startsWith('genres')) return genres;
+    if (uri.startsWith('upnp')) return computer;
+    if (uri.startsWith('Last_100')) return last100;
+    if (uri.startsWith('radio')) return radio;
+    return album;
+  }
+
   @override
   Widget build(BuildContext context) {
     debugPrint("Build");
@@ -48,7 +70,7 @@ class BrowserWidget extends StatelessWidget {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
-                      const Icon(Icons.album_outlined),
+                      iconmap(list[index]),
                       const SizedBox(width: 10),
                       Text(list[index]['name']),
                       const Spacer(),
