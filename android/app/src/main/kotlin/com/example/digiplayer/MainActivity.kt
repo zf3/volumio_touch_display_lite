6ec2d6d3f9270c77a8d27bbe988ac32ba82e97d5
Feng Zhou <fengzhou@gmail.com>
2021-12-11 21:43:57 +0800

    Cleanup

--- android/app/src/main/kotlin/com/example/digiplayer/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.digiplayer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/fvolumio/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fvolumio
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
